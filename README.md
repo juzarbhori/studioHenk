97f659c3e7a95bcbbfe9f1453bdba76d1f618ca2
Malay Mishra <malay@kredx.com>
2021-12-30 00:13:30 +0530

    FIRST commit

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# studioHenk
-
-Test
+# studoHenk
---
+++ src/dataMock.js
@@ -0,0 +1,364 @@
+export const dataMocks = {
+  step1: {
+    step: "Step 1",
+    value: "step_1",
+    title: "Choose the shape of your tabletop",
+    default: 2,
+    options: [
+      {
+        id: 1,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Rectangular",
+      },
+      {
+        id: 2,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_oval.jpg",
+        text: "Oval",
+      },
+      {
+        id: 3,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_round.jpg",
+        text: "Round",
+      },
+    ],
+  },
+  step2: {
+    step: "Step 2",
+    value: "step_2",
+    title: "Choose the finish of your tabletop",
+    default: 4,
+    options: [
+      {
+        id: 4,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Oak hardwax oil natural light 3041",
+      },
+      {
+        id: 5,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Oak hardwax oil natural light 3041",
+      },
+      {
+        id: 6,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_round.jpg",
+        text: "Oak hardwax oil natural light 3041",
+      },
+    ],
+  },
+  step3: {
+    step: "Step 3",
+    value: "step_3",
+    title: "Choose the edge finish of your tabletop",
+    default: 8,
+    options: [
+      {
+        id: 7,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Standered",
+      },
+      {
+        id: 8,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Rounded",
+      },
+    ],
+  },
+  step4: {
+    step: "Step 4",
+    value: "step_4",
+    title: "Choose the size of your tabletop",
+    type: "select",
+    options: [
+      {
+        title: "Length",
+        default: "160cm",
+        value: "length",
+        subOptions: [
+          {
+            value: "120cm",
+            label: "120cm",
+          },
+          {
+            value: "160cm",
+            label: "160cm",
+          },
+          {
+            value: "80cm",
+            label: "80cm",
+          },
+        ],
+      },
+      {
+        title: "Width",
+        default: "80cm",
+        value: "width",
+        subOptions: [
+          {
+            value: "260cm",
+            label: "260cm",
+          },
+          {
+            value: "90cm",
+            label: "90cm",
+          },
+          {
+            value: "60cm",
+            label: "60cm",
+          },
+        ],
+      },
+      {
+        title: "Thickness",
+        default: "20cm",
+        value: "thickness",
+        subOptions: [
+          {
+            value: "20cm",
+            label: "20cm",
+          },
+          {
+            value: "10cm",
+            label: "10cm",
+          },
+          {
+            value: "12cm",
+            label: "12cm",
+          },
+        ],
+      },
+    ],
+  },
+  step5: {
+    step: "Step 5",
+    value: "step_5",
+    title: "Choose your type of frame",
+    default: 10,
+    options: [
+      {
+        id: 10,
+        img: "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        text: "Butterfly",
+      },
+      {
+        id: 11,
+        img: "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        text: "Slim X type",
+      },
+    ],
+  },
+  step6: {
+    step: "Step 6",
+    value: "step_6",
+    title: "Choose the finish of your frame",
+    options: [
+      {
+        id: 12,
+        img: "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        text: "Rounded",
+      },
+      {
+        id: 13,
+        img: "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        text: "Slim X type",
+      },
+    ],
+  },
+};
+
+export const dataMocks2 = {
+  step1: {
+    step: "Step 1",
+    value: "step_1",
+    title: "Choose the shape of your tabletop",
+    default: 2,
+    options: [
+      {
+        id: 1,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Rectangular",
+      },
+      {
+        id: 2,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_oval.jpg",
+        text: "Oval",
+      },
+      {
+        id: 3,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_round.jpg",
+        text: "Round",
+      },
+    ],
+  },
+  step2: {
+    step: "Step 2",
+    value: "step_2",
+    title: "Choose the finish of your tabletop",
+    default: 4,
+    options: [
+      {
+        id: 4,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Oak hardwax oil natural light 3041",
+      },
+      {
+        id: 5,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Oak hardwax oil natural light 3041",
+      },
+    ],
+  },
+  step3: {
+    step: "Step 3",
+    value: "step_3",
+    title: "Choose the edge finish of your tabletop",
+    default: 8,
+    options: [
+      {
+        id: 7,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Standered",
+      },
+      {
+        id: 8,
+        img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+        text: "Rounded",
+      },
+    ],
+  },
+  step4: {
+    step: "Step 4",
+    value: "step_4",
+    title: "Choose the size of your tabletop",
+    type: "select",
+    options: [
+      {
+        title: "Length",
+        default: "160cm",
+        value: "length",
+        subOptions: [
+          {
+            value: "120cm",
+            label: "120cm",
+          },
+          {
+            value: "160cm",
+            label: "160cm",
+          },
+          {
+            value: "80cm",
+            label: "80cm",
+          },
+        ],
+      },
+      {
+        title: "Width",
+        default: "80cm",
+        value: "width",
+        subOptions: [
+          {
+            value: "260cm",
+            label: "260cm",
+          },
+          {
+            value: "90cm",
+            label: "90cm",
+          },
+          {
+            value: "60cm",
+            label: "60cm",
+          },
+        ],
+      },
+      {
+        title: "Thickness",
+        default: "20cm",
+        value: "thickness",
+        subOptions: [
+          {
+            value: "20cm",
+            label: "20cm",
+          },
+          {
+            value: "10cm",
+            label: "10cm",
+          },
+          {
+            value: "12cm",
+            label: "12cm",
+          },
+        ],
+      },
+    ],
+  },
+  step5: {
+    step: "Step 5",
+    value: "step_5",
+    title: "Choose your type of frame",
+    default: 10,
+    options: [
+      {
+        id: 10,
+        img: "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        text: "Butterfly",
+      },
+      {
+        id: 11,
+        img: "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        text: "Slim X type",
+      },
+    ],
+  },
+  step6: {
+    step: "Step 6",
+    value: "step_6",
+    title: "Choose the finish of your frame",
+    options: [
+      {
+        id: 12,
+        img: "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        text: "Rounded",
+      },
+      {
+        id: 13,
+        img: "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0",
+        text: "Slim X type",
+      },
+    ],
+  },
+};
+
+export const priceData = {
+  newAmount: "Rs 1940",
+  material: "OAK",
+  edgeFinish: "Standered",
+  image: [
+    {
+      id: 1,
+      img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_rectangular.jpg",
+    },
+    {
+      id: 2,
+      img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_oval.jpg",
+    },
+    {
+      id: 3,
+      img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_round.jpg",
+    },
+    {
+      id: 4,
+      img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_oval.jpg",
+    },
+    {
+      id: 5,
+      img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_round.jpg",
+    },
+    {
+      id: 6,
+      img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_oval.jpg",
+    },
+    {
+      id: 7,
+      img: "https://s3-eu-west-1.amazonaws.com/studio-henk-live/assets/Images/configurator/shape-new/configurator_symbol_tabletop_round.jpg",
+    },
+  ],
+};
